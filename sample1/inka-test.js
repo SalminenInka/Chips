465f3f9a75add9ba9f1d3754709d1cdeba119db9
inka <inka_salminen@hotmail.com>
2022-02-07 16:17:27 +0200

    Ratkaisematon tehtävä

---
+++ sample1/inka-test.js
@@ -0,0 +1,9 @@
+// 01010011 binääri desimaaliin
+
+let pueSakarilleVillapaita = 'joo';
+if (pueSakarilleVillapaita === 'joo') {
+    console.log('Hihhihhii, kutittaa:D')
+} else {
+    console.log('Hävisit pelin.')
+}; 
+
--- sample1/test2.js
+++ sample1/test2.js
@@ -7,4 +7,28 @@ for (i = 0; i < bigthings.length; i++) {
         if (letter === 'e' || letter === 'p') letterEorP++;
        // console.log(bigboi.charAt(j))
     }};
-    console.log(letterEorP);
+ console.log(letterEorP);
+
+
+
+const names = ['Inka', 'Valtteri', 'Jasmin', 'Paki', 'Katri', 'Ukemi', 'Mauku', 'Maikkuli', 'Hilipatipippan', 'Make'];
+let expectedK = names[3].charAt(2)
+let order = names.sort();
+let expectedT = order[3].charAt(2)
+if (expectedK === 'k' && expectedT === 'b') {
+    console.log('yes');
+} else {
+    console.log('no')
+};
+
+const harjoitus1 = function(a, b) {
+    return a - b;
+}
+console.log(harjoitus1(10,7));
+
+// 01010011 binääri desimaaliin
+function binToDec(bstr) {
+    return parseInt ((bstr + '')
+    .replace(/[^01]/gi, ''), 2);
+};
+console.log(binToDec('01010011'));
