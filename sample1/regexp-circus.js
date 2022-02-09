162c4a8f7f24d26ba883d1654a79161cb464165e
Paci Salminen <pasi.salminen@cgi.com>
2022-02-09 16:27:59 +0200

    Added more stuff to regexp-circus.js

--- sample1/regexp-circus.js
+++ sample1/regexp-circus.js
@@ -1,4 +1,4 @@
-const reBin = /^(0|1)+$/;
+const reBin = /(0|1)+/;
 const reDec = /^[0-9]+$/;
 const reOct = /^[0-7]+$/;
 const reHex = /^[0-9A-F]+$/i;
@@ -9,4 +9,22 @@ const reHex = /^[0-9A-F]+$/i;
     if (luku.match(reOct)) console.log(`Luku ${luku} voi olla oktaali`);
     if (luku.match(reDec)) console.log(`Luku ${luku} voi olla desimaali`);
     if (luku.match(reHex)) console.log(`Luku ${luku} voi olla hexadesimaali`);
+  });
+
+// Nyt alkaa hevi setti. Debuggaa, debuggaa ja kysele tästä.
+const patterns = new Map([[2, /^(0|1)+$/], [8, /^[0-7]+$/], [10, /^[0-9]+$/], [16, /^[0-9A-F]+$/i]]);
+
+[
+  { radix: 2, luku: '10101010' },
+  { radix: 2, luku: '76987098' },
+  { radix: 8, luku: '32321321' },
+  { radix: 10, luku: '878afd' },
+  { radix: 16, luku: 'DEAD' },
+  { radix: 2, luku: 'BEEF' },
+]
+  .forEach((testattava) => {
+    const { radix, luku } = testattava;
+    const re = patterns.get(radix);
+    if (luku.match(re)) console.log(`Value ${luku} matches regex defined for radix ${radix}`);
+    else console.log(`Value ${luku} does NOT match regex defined for radix ${radix}`);
   });
