b423a2df267dfc9619894efb0b335607b614e86b
Paci Salminen <pasi.salminen@cgi.com>
2022-02-15 19:51:15 +0200

    Serious exorcism

--- sample1/.eslintrc.js
+++ sample1/.eslintrc.js
@@ -1,8 +1,18 @@
+/* eslint-disable linebreak-style */
 module.exports = {
   extends: 'airbnb-base',
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  globals: {
+    it: true,
+    describe: true,
+    before: true,
+  },
   rules: {
     'no-cond-assign': [2, 'except-parens'],
-    'linebreak-style': 'off',
     'no-console': 'off',
+    'lines-between-class-members': ['error', { exceptAfterSingleLine: true }],
   },
 };
