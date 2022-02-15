a41e64734740ea1e22ea581e159be9e8678b56ba
Paci Salminen <pasi.salminen@cgi.com>
2022-02-15 20:24:55 +0200

    Uh

--- sample1/.eslintrc.js
+++ sample1/.eslintrc.js
@@ -1,17 +1,27 @@
-/* eslint-disable linebreak-style */
 module.exports = {
   extends: 'airbnb-base',
-  parser: 'babel-eslint',
+  parser: '@babel/eslint-parser',
   parserOptions: {
     ecmaVersion: 2020,
+    requireConfigFile: false,
   },
   globals: {
     it: true,
     describe: true,
-    before: true,
   },
   rules: {
     'no-cond-assign': [2, 'except-parens'],
-    'no-console': 'off',
+    // 'no-console': 'off',
+    'lines-between-class-members': [
+      'error', 'always', { exceptAfterSingleLine: true },
+    ],
+    'import/no-extraneous-dependencies': [
+      'error', {
+        devDependencies: false,
+        optionalDependencies: false,
+        peerDependencies: false,
+      },
+    ],
+    'import/extensions': 0,
   },
 };
